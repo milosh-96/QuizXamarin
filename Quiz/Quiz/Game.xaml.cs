26ad529dad71d830ab22b0d33f2c78e0f7f17cb5
milosh-96 <milosjovanovic042@gmail.com>
2019-05-23 16:09:40 +0200

    Work on Game Page

--- Quiz/Quiz/Game.xaml.cs
+++ Quiz/Quiz/Game.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +13,28 @@ namespace Quiz
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Game : ContentPage
     {
-        List<Models.Question> Questions = new List<Models.Question>() {
+        ObservableCollection<Models.Question> Questions = new ObservableCollection<Models.Question>() {
                 new Models.Question("Sport","single","1","Koji tim je 2010. osvojio Tripletu?","Inter",new List<string>() {"Juventus","Milan","Napoli"})
             };
+        
+        int numberOfPoints = 0;
+        public int NumberOfPoints { get => numberOfPoints; set => numberOfPoints = value;  }
+
         public Game()
         {
-
+        
             InitializeComponent();
-           
-            QuestionTitle.Text = "Question 1/10";
+            QuestionsView.ItemsSource = Questions;
+
+
+        }
+
+
+        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            Questions.Add(new Models.Question("Drama", "single", "1", "Serija", "da", new List<string>() { "aaa", "bbb", "ccc" }));
+            this.NumberOfPoints += 1;
+
         }
     }
 }
